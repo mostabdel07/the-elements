7112536a0ee5ad6df65fd364f86082db8cff592c
RaulMOAB <raulmontoroabaijon@gmail.com>
2023-03-09 20:22:38 +0100

    Responsive Design added

--- client/src/app/components/team/team.component.ts
+++ client/src/app/components/team/team.component.ts
@@ -12,31 +12,8 @@ import { StorageService } from 'src/app/services/storage.service';
 })
 export class TeamComponent implements OnInit {
   developers!: Developer[];
-  newDeveloper!: Developer;
-  description!: string;
-  showDiv: boolean = false;
-  showAddDiv: boolean = false;
-  selected_id!: any;
-  name!: string;
-  new_description!: string;
-  image!: string;
   isLoggedIn = false;
-
   webDevelopers!: Developer[];
-
-  updateForm = new FormGroup({
-    description: new FormControl(''),
-  });
-
-  addForm = new FormGroup({
-    name: new FormControl('', [
-      Validators.required,
-      Validators.pattern('[a-zA-Z]+'),
-    ]),
-    new_description: new FormControl('', [Validators.required]),
-    image: new FormControl('', [Validators.required]),
-    type: new FormControl(''),
-  });
 
   constructor(
     private developerService: DeveloperService,
@@ -47,8 +24,6 @@ export class TeamComponent implements OnInit {
   ngOnInit(): void {
     this.developers = [];
     this.webDevelopers = [];
-    (this.name = ''), (this.new_description = '');
-    this.image = '';
 
     this.developerService.getDeveloperBoard().subscribe({
       next: (data: any) => {
@@ -64,96 +39,5 @@ export class TeamComponent implements OnInit {
     });
     // Check if user is in localStorage
     this.isLoggedIn = this.storageService.isLoggedIn();
-  }
-  /**
-   * Function to update a web developer description
-   * @param id
-   */
-  updateDescription(dev: any) {
-    this.description = this.updateForm.value.description!; // lo que se ha puesto en el input
-    this.developerService
-      .updateDescription(dev.id, this.description)
-      .subscribe({
-        next: (data) => {
-          dev.description = this.description;
-          this.showDiv = false;
-        },
-        error: (err) => {
-          this.description = 'No se ha podido actualizar la descripción';
-          console.log(err);
-        },
-      });
-  }
-
-  /**
-   * Function to show update form
-   * @param id
-   */
-  displayForm(id: any) {
-    this.showAddDiv = false;
-    this.selected_id = id;
-    if (this.selected_id !== 0) {
-      this.showDiv = true;
-    } else {
-      this.showDiv = false;
-    }
-  }
-
-  /**
-   * Function to show add form
-   * @param id
-   */
-  displayAddForm(id: any) {
-    this.showDiv = false;
-    this.selected_id = id;
-    if (this.selected_id !== 0) {
-      this.showAddDiv = true;
-    } else {
-      this.showAddDiv = false;
-    }
-  }
-
-  closeForm() {
-    this.selected_id = 0;
-  }
-
-  /**
-   * Function to add a new web developer
-   */
-  addWebDeveloper() {
-    console.log(this.developers.length); //7
-    let id = this.developers.length + 1;
-    this.newDeveloper = new Developer(
-      id,
-      this.addForm.value.name!,
-      this.addForm.value.new_description!,
-      `/images/devs/${this.addForm.value.image}.jpg`
-    );
-    console.log(this.newDeveloper);
-    this.developerService.addDeveloper(this.newDeveloper).subscribe({
-      next: (data) => {
-        this.webDevelopers.push(this.newDeveloper);
-        this.showAddDiv = false;
-      },
-      error: (err) => {
-        console.log('no se ha añadido correctamente');
-      },
-    });
-  }
-
-  deleteDeveloper(id: any) {
-    console.log(id);
-    this.developerService.deleteDeveloper(id).subscribe({
-      next: (data) => {
-        const aux_developers = this.webDevelopers.filter((dev) => {
-          return dev.id !== id;
-        });
-
-        this.webDevelopers = aux_developers;
-      },
-      error: (err) => {
-        console.log('no se ha podido eliminar');
-      },
-    });
   }
 }
